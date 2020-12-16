12b0dbbdd4286e6f6c171b0fd04b17f262fcdd5a
Isaiah <anyiminene@gmail.com>
2020-12-16 13:42:14 -0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "./components/Sidebar";
+import Display from "./components/Display";
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
